76bc2b7259e9bb0ce3c2e8604e517176f2870f32
DieOld <rikepic@gmail.com>
2019-09-22 11:42:26 +0400

    remove files

--- python/django/studentControl/main/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-
-from main import views
-
-urlpatterns = [
-    path("", views.Index.as_view(), name='index'),
-    path("search/", views.Search.as_view(), name='search')
-]
--- python/snake.py
+++
@@ -1,8 +0,0 @@
-import random
-import curses
-
-s = curses.initsrc()
-curses.curs_set(0)
-sh, sw = s.getmaxyx()
-w = curses.newwin(sh,sw,0,0)
-

4a359547f28eb3ad4b90e818e5baae9e2780a85c
Vadim <vadym.tishchenko@nure.ua>
2019-09-24 01:06:04 +0300

    student profile

--- python/django/student_controll(2.0)/student/views.py
+++ python/django/student_controll(2.0)/student/views.py
@@ -7,7 +7,7 @@ from main.models import Student
 class StudentProfile(generic.ListView):
     template_name = 'profile.html'
     model = Student
-    context_object_name = 'students'
+    context_object_name = 'student'
 
     def get_queryset(self):
         return self.model.objects.filter(user=self.request.user)
